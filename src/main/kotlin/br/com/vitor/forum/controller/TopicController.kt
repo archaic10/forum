a350d5dc89c3cf714112624171fc0c375f538384
Vitor <vitor.franca.9400@gmail.com>
2025-02-12 15:24:04 -0300

    feat: finish crud

--- src/main/kotlin/br/com/vitor/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/vitor/forum/controller/TopicController.kt
@@ -1,32 +1,48 @@
 package br.com.vitor.forum.controller
 
 import br.com.vitor.forum.dto.form.TopicForm
+import br.com.vitor.forum.dto.form.UpdateTopicForm
 import br.com.vitor.forum.dto.view.TopicView
 import br.com.vitor.forum.service.TopicService
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/topics")
-class TopicController (private val service: TopicService) {
+class TopicController(private val service: TopicService) {
     @GetMapping
     fun list(): List<TopicView> {
-       return service.list();
+        return service.list();
     }
 
     @GetMapping("/{id}")
-    fun searchById(@PathVariable id:Long):TopicView {
+    fun searchById(@PathVariable id: Long): TopicView {
         return service.searchById(id);
     }
 
     @PostMapping
-    fun register(@RequestBody @Valid dto: TopicForm) {
-        service.register(dto);
+    fun register(
+        @RequestBody @Valid dto: TopicForm,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<TopicView> {
+        val topicView = service.register(dto)
+        val uri = uriBuilder.path("/topics/${topicView.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicView)
+    }
+
+    @PutMapping
+    fun update(@RequestBody @Valid dto: UpdateTopicForm):ResponseEntity<TopicView> {
+        val topicView = service.update(dto)
+        return ResponseEntity.ok(topicView)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Long) {
+        service.delete(id)
     }
 
 }
--- src/main/kotlin/br/com/vitor/forum/service/TopicService.kt
+++ src/main/kotlin/br/com/vitor/forum/service/TopicService.kt
@@ -1,6 +1,7 @@
 package br.com.vitor.forum.service
 
 import br.com.vitor.forum.dto.form.TopicForm
+import br.com.vitor.forum.dto.form.UpdateTopicForm
 import br.com.vitor.forum.dto.view.TopicView
 import br.com.vitor.forum.mapper.view.TopicViewMapper
 import br.com.vitor.forum.mapper.form.TopicFormMapper
@@ -14,6 +15,7 @@ class TopicService(
     private var topics: List<Topic> = ArrayList(),
     private val topicViewMapper: TopicViewMapper,
     private val topicFormMapper: TopicFormMapper,
+    private val curseService: CurseService,
 ) {
 
 
@@ -22,16 +24,43 @@ class TopicService(
     }
 
     fun searchById(id: Long): TopicView {
-        val topic = topics.stream().filter({ t ->
+        val topic = topics.stream().filter{ t ->
             t.id == id
-        }).findFirst().get();
+        }.findFirst().get();
 
         return topicViewMapper.map(topic)
     }
 
-    fun register(dto: TopicForm) {
+    fun register(dto: TopicForm): TopicView {
         val topic = topicFormMapper.map(dto)
         topic.id = topics.size.toLong() +1
-        topics = topics.plus(topic);
+        topics = topics.plus(topic)
+        return topicViewMapper.map(topic)
+    }
+
+    fun update(dto: UpdateTopicForm):TopicView {
+        val topic = topics.stream().filter{ t ->
+            t.id == dto.id
+        }.findFirst().get()
+
+        val topicUpdated = Topic(
+            id = dto.id,
+            title = dto.title,
+            message = dto.message,
+            author = topic.author,
+            curse = topic.curse,
+            response = topic.response,
+            status = topic.status,
+            dateCreated = topic.dateCreated
+        )
+        topics = topics.minus(topic).plus(topicUpdated)
+        return topicViewMapper.map(topicUpdated)
+    }
+
+    fun delete(id: Long) {
+        val topic = topics.stream().filter{ t ->
+            t.id == id
+        }.findFirst().get()
+        topics = topics.minus(topic)
     }
 }
